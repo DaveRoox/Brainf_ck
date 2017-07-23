995198dec1862ed8e0094cc1aa5df215ec3e13be
Davide Russo <daviderusso@mbp-di-davide-2.station>
2017-07-23 11:06:27 +0200

    Implementazione interprete

--- Brainf_ck/ViewController.swift
+++ Brainf_ck/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol IOStreamDelegate {
+    
+    func output(value: ValueType)
+    func input() -> ValueType?
+    
+}
 
+class ViewController: UIViewController, IOStreamDelegate {
+    
+    var brainfuckInterpreter : Brainf_ckInterpreter?
+
+    @IBOutlet weak var programTextField: UITextField!
+    @IBOutlet weak var outputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.brainfuckInterpreter = Brainf_ckInterpreter(iostreamDelegate: self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func execute(_ sender: UIButton) {
+        self.brainfuckInterpreter?.execute(program: self.programTextField.text)
+    }
 
+    @IBAction func clear(_ sender: UIButton) {
+        self.outputTextField.text = ""
+        self.brainfuckInterpreter = Brainf_ckInterpreter(iostreamDelegate: self)
+    }
+    
+    func input() -> ValueType? {
+        return nil
+    }
+    
+    func output(value: ValueType) {
+        self.outputTextField.text = self.outputTextField.text! + "\(value)"
+    }
 }
 
